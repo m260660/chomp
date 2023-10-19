cd9c07b17017385de2c914051fd1a4704824e25f
m260660 <m260660@usna.edu>
2023-10-19 14:45:10 -0400

    game2

--- game2.py
+++ game2.py
@@ -9,13 +9,11 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 tile_size = 64
-#
-# #define colors
-# BLUE = (0,0,255)
-# BROWN = (224,161,52)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Beech")
+
+# green_fish = pygame.transform.flip(green_fish,True,False)
 
 #load game font
 custom_font = pygame.font.Font("assets/fonts/Brainfish_Rush.ttf", size=128)
@@ -47,16 +45,28 @@ def draw_bg(surf):
     text = custom_font.render('chomp', True, (255,0,0))
     surf.blit(text, (screen_width/2 - text.get_width()/2,screen_height/2 - text.get_height()/2))
 
-    #draw fishes
-    def draw_fishes
+#draw fishes
+def draw_fishes(surf):
+    #load fish tiles from sprites
+    green_fish = pygame.image.load("assets/sprites/green_fish.png").convert()
+    green_fish.set_colorkey((0,0,0)) #png transparency
+    green_fish = pygame.transform.flip(green_fish, True, False)
+
+    for _ in range(5):
+        x = random.randint(0, screen_width-tile_size)
+        y = random.randint(0, screen_width-tile_size)
+        surf.blit(green_fish,(x,y))
+
 #Main loop
 running = True
 background = screen.copy()
 draw_bg(background)
+draw_fishes(background)
 
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.quit:
+        #print(event)
+        if event.type == pygame.QUIT:
             running = False
     #draw bg
     screen.blit(background, (0,0))
@@ -64,4 +74,4 @@ while running:
     # update display
     pygame.display.flip()
 
-pygame.quit
+pygame.quit()
